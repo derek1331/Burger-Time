2ce688cc3e2b322488e9bd906faeab8b9693cfd7
Derek Reed <1331derek@gmail.com>
2018-08-25 20:12:15 -0600

    css fixes

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krabby Patty', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Good Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Paunch Burger', FALSE);
